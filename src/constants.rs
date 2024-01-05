8834701fef50dbe9861e3f8dba6269e16a0d753a
Aravind I M <aravindim@yahoo.com>
2024-01-06 00:13:06 +0530

    feat(constants): added new constants added XSM_WORD_SIZE, XSM_MEMORY_NUM_PAGES, XSM_PAGE_SIZE, XSM_REG_SIZE, XSM_NUM_REG, XSM_INSTRUCTION_SIZE

---
+++ src/constants.rs
@@ -0,0 +1,7 @@
+pub const XSM_WORD_SIZE: u32 = 16;
+pub const XSM_MEMORY_NUM_PAGES: u32 = 128;
+pub const XSM_PAGE_SIZE: u32 = 512;
+pub const XSM_REG_SIZE: u32 = XSM_WORD_SIZE;
+pub const XSM_NUM_REG: u32 = 33;
+
+pub const XSM_INSTRUCTION_SIZE: u32 = 2;
