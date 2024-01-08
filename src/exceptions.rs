bae4419342f5766cbc9941e912998bf09c6fa21f
Aravind I M <aravindim@yahoo.com>
2024-01-08 13:43:17 +0530

    fix(exceptions): remove invalid fields in exception types

--- src/exceptions.rs
+++ src/exceptions.rs
@@ -1,26 +1,20 @@
 pub enum Exception {
     PageFault {
         pub message: String,
-        pub mem_addr: u32,
         pub page_num: u32,
         pub cpu_mode: u32,
     },
     IllegalInstruction {
         pub message: String,
-        pub mem_addr: u32,
-        pub page_num: u32,
         pub cpu_mode: u32,
     },
     IllegalMemory {
         pub message: String,
         pub mem_addr: u32,
-        pub page_num: u32,
         pub cpu_mode: u32,
     },
     Arithmetic {
         pub message: String,
-        pub mem_addr: u32,
-        pub page_num: u32,
         pub cpu_mode: u32,
     },
 }
