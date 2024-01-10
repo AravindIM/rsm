d7d761c3ceb9a82e9721f46e2587487006e9824f
Aravind I M <aravindim@yahoo.com>
2024-01-10 21:37:56 +0530

    fix(registers): change init() to new()

--- src/registers.rs
+++ src/registers.rs
@@ -5,7 +5,7 @@ pub struct Registers {
 }
 
 impl Registers {
-    pub fn init() -> Self {
+    pub fn new() -> Self {
         let reg_names = vec![
             "R0", "R1", "R2", "R3", "R4", "R5", "R6", "R7", "R8", "R9", "R10", "R11", "R12", "R13",
             "R14", "R15", "R16", "R17", "R18", "R19", "P0", "P1", "P2", "P3", "BP_REG", "SP_REG",
