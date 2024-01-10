03baa8745d18fe373a54a24e974a91c261ef5a16
Aravind I M <aravindim@yahoo.com>
2024-01-10 12:49:41 +0530

    feat(register): add get method for register

--- src/registers.rs
+++ src/registers.rs
@@ -18,4 +18,10 @@ impl Registers {
 
         Registers { registers: reg_map }
     }
+    pub fn get(self, name: &str) -> Result<Word, String> {
+        match self.registers.get(name) {
+            Some(w) => Ok(w.clone()),
+            None => Err(format!("Invalid register {name} was queried for value")),
+        }
+    }
 }
