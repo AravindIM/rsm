403160e3f53d7146de1c6715fc567977f851a9de
Aravind I M <aravindim@yahoo.com>
2024-01-20 12:23:18 +0530

    fix(constants): change constant names

--- src/constants.rs
+++ src/constants.rs
@@ -1,7 +1,7 @@
-pub const XSM_WORD_SIZE: u32 = 16;
-pub const XSM_MEMORY_NUM_PAGES: u32 = 128;
-pub const XSM_PAGE_SIZE: u32 = 512;
-pub const XSM_REG_SIZE: u32 = XSM_WORD_SIZE;
-pub const XSM_NUM_REG: u32 = 33;
+pub const RSM_WORD_SIZE: usize = 16;
+pub const RSM_MEMORY_NUM_PAGES: usize = 128;
+pub const RSM_PAGE_SIZE: usize = 512;
+pub const RSM_REG_SIZE: usize = RSM_WORD_SIZE;
+pub const RSM_NUM_REG: usize = 33;
 
-pub const XSM_INSTRUCTION_SIZE: u32 = 2;
+pub const RSM_INSTRUCTION_SIZE: usize = 2;
