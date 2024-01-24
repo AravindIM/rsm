92c688564ae5916028969b442f104f6f6c90051d
Aravind I M <aravindim@yahoo.com>
2024-01-24 18:56:56 +0530

    feat(constants): added RSM_MEMORY_NUM_WORDS

--- src/constants.rs
+++ src/constants.rs
@@ -1,6 +1,7 @@
 pub const RSM_WORD_SIZE: usize = 16;
 pub const RSM_MEMORY_NUM_PAGES: usize = 128;
 pub const RSM_PAGE_SIZE: usize = 512;
+pub const RSM_MEMORY_NUM_WORDS: usize = RSM_MEMORY_NUM_PAGES * RSM_PAGE_SIZE;
 pub const RSM_DISK_BLOCKSIZE: usize = RSM_PAGE_SIZE;
 pub const RSM_REG_SIZE: usize = RSM_WORD_SIZE;
 pub const RSM_NUM_REG: usize = 33;
